d027e68d669a4f7ba9f1a0a37385856870854f32
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-07-12 13:53:17 -0700

    Add comment for desugar on library

--- lib/src/main/java/com/example/hello/helloeither/EitherLibUtil.java
+++ lib/src/main/java/com/example/hello/helloeither/EitherLibUtil.java
@@ -8,6 +8,10 @@ public class EitherLibUtil {
     return flag ? Either.right(true) : Either.left(new RuntimeException("exception"));
   }
 
+  /**
+   * A java 8 method reference in library without enable desugar passes compile.
+   * If the app is not aware of such assumption and did not enable the desugar flag, it will crash.
+   */
   public static String foldBelow23(Either<Throwable, Boolean> either) {
     Either<String, String> map = either.map(Throwable::getMessage, String::valueOf);
     return map.isLeft() ? map.getLeft() : map.getRight();
