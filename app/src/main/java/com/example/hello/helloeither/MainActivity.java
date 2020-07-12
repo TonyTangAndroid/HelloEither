422791b6295380fccd89a550fae0d4e98de99ba8
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-07-12 13:09:47 -0700

    Use Java 8

--- app/src/main/java/com/example/hello/helloeither/MainActivity.java
+++ app/src/main/java/com/example/hello/helloeither/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.spencerwi.either.Either;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +17,33 @@ public class MainActivity extends AppCompatActivity {
 
   public void click(View view) {
 
-    view.postDelayed(
-        new Runnable() {
-          @Override
-          public void run() {
-            Toast.makeText(MainActivity.this, message(), Toast.LENGTH_SHORT).show();
-          }
-        },
-        300);
+    view.postDelayed(this::showToast, 300);
+  }
+
+  private void showToast() {
+    show(message());
+  }
+
+  private void show(String message1) {
+    Toast.makeText(MainActivity.this, message1, Toast.LENGTH_SHORT).show();
   }
 
   private String message() {
-    return "clicked";
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+      String fold = either().fold(String -> "OK", String -> "NOT OK");
+      System.out.println("fold after API 23 result is :" + fold);
+      return fold;
+    } else {
+      String fold = either().fold(String -> "OK", String -> "NOT OK 23");
+      System.out.println("fold before API 23 result is :" + fold);
+      return fold;
+    }
+  }
+
+  private Either<Boolean, Boolean> either() {
+    boolean flag = new Random().nextBoolean();
+    Either<Boolean, Boolean> result = flag ? Either.right(true) : Either.left(false);
+    System.out.println("either result is right:" + result.isRight());
+    return result;
   }
 }
