5131e916e24b5004cd0ddc84b9208f01ce3cc11f
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-07-12 13:08:42 -0700

    Add java 7 style code

--- app/src/main/java/com/example/hello/helloeither/MainActivity.java
+++ app/src/main/java/com/example/hello/helloeither/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hello.helloeither;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void click(View view) {
+
+    view.postDelayed(
+        new Runnable() {
+          @Override
+          public void run() {
+            Toast.makeText(MainActivity.this, message(), Toast.LENGTH_SHORT).show();
+          }
+        },
+        300);
+  }
+
+  private String message() {
+    return "clicked";
   }
 }
